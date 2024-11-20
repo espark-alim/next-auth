68b4f2220e3f9793f5d0cf9826cdeae166d9f77e
alim@espark <ali@esparkconsultants.com>
2024-11-20 17:46:57 +0500

    redirect to /

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,7 +8,18 @@ const handler = NextAuth({
             clientSecret: process?.env?.GITHUB_SECRET
         })
     ],
-    secret: process?.env?.AUTH_SECRET
-})
+    secret: process?.env?.AUTH_SECRET,
+    debug: true,
+    callbacks: {
+        async redirect({ url, baseUrl }) {
+            if (url?.startsWith("/")) {
+                return `${baseUrl}${url}`;
+            } else if (url?.startsWith(baseUrl)) {
+                return url;
+            }
+            return `${baseUrl}/dashboard`;
+        },
+    },
+});
 
-export { handler as GET, handler as POST, }
+export { handler as GET, handler as POST };
