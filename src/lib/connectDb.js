c52f4845a3bd9c89a4ca32e00fc6878dad0d37e4
alim <ali@esparkconsultants.com>
2024-11-22 00:20:41 +0500

    .

--- src/lib/connectDb.js
+++ src/lib/connectDb.js
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
 
 const connectDb = async () => {
+  MONGO_URI="mongodb+srv://ali:mongo123@next-cluster.ixj4y.mongodb.net/?retryWrites=true&w=majority&appName=next-Cluster"
+
   if (mongoose.connection.readyState === 1) {
     console.log('Using existing database connection');
     return mongoose.connection.asPromise();
   }
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI);
-    
+    const db = await mongoose.connect(MONGO_URI);
+
     console.log('Database connected');
     return db;
   } catch (error) {
